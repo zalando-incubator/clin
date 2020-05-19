f0a47afdbf5bafb814888decfe8b3972db13eb15
Daniel Stockhammer <daniel.stockhammer@zalando.de>
2020-05-19 17:51:57 +0200

    remove failing tests (trust me, I'm an engineer)

--- tests/test_package.py
+++ tests/test_package.py
@@ -6,6 +6,3 @@ from clin import __version__
 def test_pyproject_version_matches_package_version():
     pyproj = toml.load("pyproject.toml")
     assert __version__ == pyproj["tool"]["poetry"]["version"]
-
-def test_this_test_fails():
-    assert "lorem" == "ipsum"

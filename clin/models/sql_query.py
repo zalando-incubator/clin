f039056a2abaac29801adf57c5e935a2778d2653
Amr Abdelshafi <amrayoub97@gmail.com>
2022-07-18 10:52:25 +0200

    Reformatted

--- clin/models/sql_query.py
+++ clin/models/sql_query.py
@@ -28,7 +28,7 @@ class OutputEventType:
             if "repartitioning" in spec
             else None,
             cleanup=Cleanup.from_spec(spec["cleanup"]),
-            partition_compaction_key_field=spec["partitionCompactionKeyField"]
+            partition_compaction_key_field=spec["partitionCompactionKeyField"],
         )
 
     def to_spec(self) -> dict[str, any]:
